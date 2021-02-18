9dc3ce9b280c4f4cd69086e36b1c849ff0d6c534
Camila Ferraz <camicferraz@hotmail.com>
2021-02-18 19:02:20 -0300

    Added a logic to check the digits

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function convert(){
+function convert() {
   const binary = document.getElementById("b_Input").value
 
   binary.split('').map((char) => {
-    if(char !== '1' && char !== '0') return alert('Please enter a valid binary')
+    if (char !== '1' && char !== '0') return alert('Please enter a valid binary')
   })
-  
-  const decimal = parseInt(binary, 2)
-  document.getElementById("d_Output").value = decimal
-  return true
+
+  if (binary.length <= 8) {
+    const decimal = parseInt(binary, 2)
+    document.getElementById("d_Output").value = decimal
+    return true
+  }
+  else{
+    return alert("Please enter 8 or less digits")
+  }
 }
